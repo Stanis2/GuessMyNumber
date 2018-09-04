9bc783329cd7945fb729dc322a1c86d6a84aa378
Stanis2 <stanis2@inbox.lv>
2018-09-04 19:40:43 +0300

    TSI - 04.09.2018

--- src/tsi/stanislaw/gmn/Main.java
+++ src/tsi/stanislaw/gmn/Main.java
@@ -9,47 +9,59 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Начать игру? Да или Нет.");
-        String answer = scan.next();
+        System.out.println("Начать игру?");
+        String answer = ask();
 
         if (answer.equals("Нет")){
             System.exit(0);
-        }
+        } else if (answer.equals("Да")) {
 
-        do {
+            do {
 
-            int myNum = rand.nextInt(100) + 1;
-            System.out.println(myNum);
-            boolean userLost = true;
-            boolean userWin = false;
+                int myNum = rand.nextInt(100) + 1;
+                System.out.println(myNum);
+                boolean userLost = true;
+                boolean userWin = false;
 
-            for (int i = 1; i < 11; i++) {
-                System.out.println("Попытка #" + i);
-                int userNum = scan.nextInt();
+                for (int i = 1; i < 11; i++) {
+                    System.out.println("Попытка #" + i);
+                    int userNum = scan.nextInt();
 
-                if (userNum > myNum) {
-                    System.out.println("Много!");
-                } else if (userNum < myNum) {
-                    System.out.println("Мало!");
-                } else {
-                    System.out.println("Угадал!");
-                    userLost = false;
-                    userWin = true;
-                    System.out.println("Играть еще раз? Да или Нет.");
-                    i = 11;
+                    if (userNum > myNum) {
+                        System.out.println("Много!");
+                    } else if (userNum < myNum) {
+                        System.out.println("Мало!");
+                    } else {
+                        System.out.println("Угадал!");
+                        userLost = false;
+                        userWin = true;
+                        break;
+                    }
                 }
-            }
 
-            if (userLost) {
-                System.out.println("Попробовать еще раз? Да или Нет.");
-                answer = scan.next();
-            }
+                if (userLost) {
+                    System.out.println("Попробовать еще раз?");
+                    answer = ask();
+                }
 
-            if (userWin) {
-                answer = scan.next();
-            }
+                if (userWin) {
+                    System.out.println("Играть еще раз?");
+                    answer = ask();
+                }
 
-        } while (answer.equals("Да"));
+            } while (answer.equals("Да"));
+        } else {
+            System.out.println("Ошибка ввода!");
+            System.exit(0);
+        }
+    }
 
+    static String ask(){
+        String answer;
+        do {
+            System.out.println("Да или Нет");
+            answer = scan.next();
+        } while (!answer.equals("Да") && !answer.equals("Нет"));
+        return answer;
     }
 }
